47ebe34f98822e0ea2f37fb5726bf9d9a24787fb
Tim Williams <tim@wordery.com>
2016-07-24 20:01:53 +0100

    More website progress

--- src/routes.php
+++ src/routes.php
@@ -8,22 +8,36 @@ $app->get('/about', function ($request, $response, $args) {
     return $this->view->render($response, 'about.twig');
 })->setName('about');
 
+$app->get('/contact', function ($request, $response, $args) {
+    return $this->view->render($response, 'contact.twig');
+})->setName('contact');
+
+$app->post('/contact', function ($request, $response, $args) {
+    return $this->view->render($response, 'contact.twig');
+})->setName('contact_post');
+
 $app->group('/services', function(){
     $this->get('', function ($request, $response, $args) {
         return $this->view->render($response, 'services.twig');
     })->setName('services');
-    $this->get('/online-pt', function ($request, $response, $args) {
-        return $this->view->render($response, 'services/online-pt.twig');
-    })->setName('online-pt');
+    $this->get('/virtual-pt', function ($request, $response, $args) {
+        return $this->view->render($response, 'services/virtual-pt.twig');
+    })->setName('virtual-pt');
     $this->get('/personal-training', function ($request, $response, $args) {
         return $this->view->render($response, 'services/personal-training.twig');
     })->setName('personal-training');
     $this->get('/group-training', function ($request, $response, $args) {
-        return $this->view->render($response, 'serivces/group-training.twig');
+        return $this->view->render($response, 'services/group-training.twig');
     })->setName('group-training');
     $this->get('/injury-prevention', function ($request, $response, $args) {
         return $this->view->render($response, 'services/injury-prevention.twig');
     })->setName('injury-prevention');
+    $this->get('/sports-conditioning', function ($request, $response, $args) {
+        return $this->view->render($response, 'services/sports-conditioning.twig');
+    })->setName('sports-conditioning');
+    $this->get('/nutrition', function ($request, $response, $args) {
+        return $this->view->render($response, 'services/nutrition.twig');
+    })->setName('nutrition');
 });
 
 // $app->get('', function($request, $response, $args){})->setName('');
