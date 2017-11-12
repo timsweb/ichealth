8f687d69bc431882ffe937f338639e056f947752
Tim Williams <tim@wordery.com>
2017-11-12 13:44:59 +0000

    Change to single page

--- src/routes.php
+++ src/routes.php
@@ -1,14 +1,16 @@
 <?php
 // Routes
 $app->get('/', function ($request, $response, $args) {
-    return $this->view->render($response, 'index.twig');
+    return $this->view->render($response, 'index-new.twig');
 });
 
 $app->get('/about', function ($request, $response, $args) {
+    return $response->withRedirect('/', 302);
     return $this->view->render($response, 'about.twig');
 })->setName('about');
 
 $app->get('/contact', function ($request, $response, $args) {
+    return $response->withRedirect('/', 302);
     return $this->view->render($response, 'contact.twig');
 })->setName('contact');
 
@@ -45,9 +47,11 @@ $app->post('/contact', function ($request, $response, $args) {
 })->setName('contact_post');
 
 $app->get('/services', function ($request, $response, $args) {
+    return $response->withRedirect('/', 302);
     return $this->view->render($response, 'services.twig');
 })->setName('services');
 
 $app->get('/health', function ($request, $response, $args) {
+    return $response->withRedirect('/', 302);
     return $this->view->render($response, 'health.twig');
 })->setName('health');
